3224f437d8c99eb7bf771a0cb468f0779b52bff3
Omar Shariff <78196064+kabeershariff@users.noreply.github.com>
2022-10-02 16:14:17 +0530

    Add files via upload

--- web-scraper/gui.py
+++ web-scraper/gui.py
@@ -2,47 +2,17 @@ import kivy
 kivy.require('2.1.0') #required kivy version
 
 from kivy.app import App
-from kivy.uix.textinput import TextInput
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
-class Layout(GridLayout):
-    #initialize
-    def __init__(self, **kwargs):
-        #calling constructor
-        super(Layout, self).__init__(**kwargs)
+
+class MyGridLayout(Widget):
         
-        #main grid column number
-        self.cols = 1
-        
-        #top grid
-        self.top_grid = GridLayout()
-        self.top_grid.cols = 2
-        self.add_widget(self.top_grid)
-
-        #widgets
-
-        #search box
-        self.search_box = TextInput(multiline = False)
-        self.top_grid.add_widget(self.search_box)
-
-        #search button
-        self.search_button = Button(text="Search")
-        self.top_grid.add_widget(self.search_button)
-        
-        self.search_button.bind(on_press=self.search)
-
-        #results here
-        self.result = TextInput(multiline =True)
-        self.add_widget(self.result)
-
     def search(self, instance):
         pass
-        #query = self.search_box.text
 
 
 class MyApp(App):
 
     def build(self):
-        return Layout()
+        return MyGridLayout()
 
