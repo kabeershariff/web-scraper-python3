fbe0d39fca12cc94817f0dc8a2fab753879adcb1
Omar Shariff <78196064+kabeershariff@users.noreply.github.com>
2022-10-02 14:28:35 +0530

    Add files via upload

--- web-scraper/gui.py
+++ web-scraper/gui.py
@@ -2,10 +2,38 @@ import kivy
 kivy.require('2.1.0') #required kivy version
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+
+class Layout(GridLayout):
+    #initialize
+    def __init__(self, **kwargs):
+        #calling constructor
+        super(Layout, self).__init__(**kwargs)
+        
+        #column number
+        self.cols = 2
+
+        #widgets
+
+        #search box
+        self.search_box = TextInput(multiline = False)
+        self.add_widget(self.search_box)
+
+        #search button
+        self.search_button = Button(text="Search")
+        self.add_widget(self.search_button)
+        
+        self.search_button.bind(on_press=self.search)
+
+    def search(self, instance):
+        query = self.search_box.text
+        print(query)
+
 
 class MyApp(App):
 
     def build(self):
-        return Label(text="Blank")
+        return Layout()
 
