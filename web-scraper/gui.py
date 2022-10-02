de3d0508e94eda555c8d306c008b6e1b31df4788
Omar Shariff <78196064+kabeershariff@users.noreply.github.com>
2022-10-02 20:35:16 +0530

    Add files via upload

--- web-scraper/gui.py
+++ web-scraper/gui.py
@@ -4,11 +4,13 @@ kivy.require('2.1.0') #required kivy version
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.uix.tabbedpanel import TabbedPanel
 
 class MyLayout(Widget):
     
     search_box = ObjectProperty(None)
-    result = ObjectProperty(None)
+    result1 = ObjectProperty(None)
+    result2 = ObjectProperty(None)
     
     def search(self):
         pass
