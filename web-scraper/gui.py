493186a78ed2738a979ff26469ca7aa754dce836
Omar Shariff <78196064+kabeershariff@users.noreply.github.com>
2022-10-02 12:36:46 +0530

    Add files via upload

--- web-scraper/gui.py
+++ web-scraper/gui.py
@@ -1,1 +1,11 @@
+import kivy
+kivy.require('2.1.0') #required kivy version
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text="Blank")
 
