ecb320654f2941d2491372669a1b0db302fca98d
Omar Shariff <78196064+kabeershariff@users.noreply.github.com>
2022-10-10 18:32:41 +0530

    Add files via upload

--- web-scraper/scrape.py
+++ web-scraper/scrape.py
@@ -6,12 +6,9 @@ from bs4 import BeautifulSoup
 
 class Start(object):
     
-    def __init__(self):
-        self.get_page()
 
-    def get_page(self):
-
-        url = input("search term : ")
+    def get_page(self, url):
+        
         response = requests.get(f'https://www.flipkart.com/search?q={url}')
         response_content = response.content
         soup = BeautifulSoup(response_content, "html.parser")
